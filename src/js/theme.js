e6ec6eadc00778a12c94f69bc9dd025f7bec39cb
Omars Alifhanovs <78239462+gone-icaruss@users.noreply.github.com>
2021-08-16 02:31:19 +0300

    16.8 02:31 theme switch

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,5 +1,5 @@
 const refs = {
-    themeBtn: document.querySelector('.theme-switch_marker'),
+    themeBtn: document.getElementById('theme-switch-toggle'),
     body: document.querySelector('body')
 };
 
@@ -8,41 +8,49 @@ const theme = {
   DARK: 'dark-theme',
 };
 
-const basicTheme = localStorage.setItem('body_theme', theme.LIGHT);
+// themeDark();
+// themeLight();
 
-const checkTheme = function () {
-    if (localStorage.getItem('check') === 'true') {
-        (refs.themeBtn.check === 'true');
-        localStorage.setItem('bodyTheme', theme.LIGHT)
-        refs.body.classList.remove('theme.LIGHT')
-        refs.body.classList.add('theme.DARK')
+refs.themeBtn.addEventListener('change', toggle => {
+    toggle.target.checked ? themeDark() : themeLight()
+});
+
+// const basicTheme = localStorage.setItem('body_theme', theme.LIGHT);
+
+function basicTheme() {
+    refs.body.classList.add(theme.LIGHT);
+    localStorage.setItem('body_theme', theme.LIGHT);
+};
+
+function checkTheme () {
+   if (localStorage.getItem('check') === 'true') {
+        (refs.themeBtn.check === true);
+        localStorage.setItem('body_theme', theme.LIGHT)
+        refs.body.classList.remove(theme.LIGHT)
+        refs.body.classList.add(theme.DARK);
     } else {
-        refs.themeBtn.check === 'false'
-    }
+       (refs.themeBtn.check === false)
+   }
 };
 
-function startTheme() {
-    refs.body.classList.add(theme.LIGHT),
-        localStorage.setItem('bodyTheme', theme.LIGHT)
-};
 
 function themeLight() {
-    if(localStorage.getItem('bodyTheme', theme.LIGHT)){
+    if(localStorage.getItem('body_theme', theme.LIGHT)){
         refs.body.classList.remove(theme.DARK)
         refs.body.classList.add(theme.LIGHT)
-        localStorage.setItem('bodyTheme', themeLIGHT)
+        localStorage.setItem('body_theme', theme.LIGHT)
         localStorage.setItem('check', true)
     }
-}
+};
 
 function themeDark() {
-    if (localStorage.getItem('bodyTheme', themeDARK)) {
+    if (localStorage.getItem('body_theme', theme.DARK)) {
         refs.body.classList.remove(theme.LIGHT)
         refs.body.classList.add(theme.DARK)
-        localStorage.setItem('bodyTheme', themeDARK)
+        localStorage.setItem('body_theme', theme.DARK)
         localStorage.setItem('check', false)
     }
-}
+};
 
 
 
